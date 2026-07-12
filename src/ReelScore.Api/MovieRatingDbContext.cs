377b7fb2432ff4716b1422d693635b3b19480f9f
Umaid Malik <umaid54321@hotmail.com>
2026-07-12 15:45:31 -0400

    feat: add initial PostgreSQL schema migration

--- src/ReelScore.Api/MovieRatingDbContext.cs
+++ src/ReelScore.Api/MovieRatingDbContext.cs
@@ -3,7 +3,7 @@ using ReelScore.Api.Models;
 
 namespace ReelScore.Api;
 
-public class MovieRatingDbContext : DbContext
+public sealed class MovieRatingDbContext : DbContext
 {
     public MovieRatingDbContext(
         DbContextOptions<MovieRatingDbContext> options)
@@ -21,28 +21,124 @@ public class MovieRatingDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>().ToTable("users");
-        modelBuilder.Entity<Movie>().ToTable("movies");
-        modelBuilder.Entity<Rating>().ToTable("ratings");
+        ConfigureUsers(modelBuilder);
+        ConfigureMovies(modelBuilder);
+        ConfigureRatings(modelBuilder);
+    }
 
-        modelBuilder.Entity<Rating>()
-            .HasIndex(rating => new
-            {
-                rating.UserId,
-                rating.MovieId
-            })
+    private static void ConfigureUsers(ModelBuilder modelBuilder)
+    {
+        var user = modelBuilder.Entity<User>();
+
+        user.ToTable("users");
+
+        user.HasKey(entity => entity.UserId);
+
+        user.Property(entity => entity.UserId)
+            .HasColumnName("user_id")
+            .ValueGeneratedOnAdd();
+
+        user.Property(entity => entity.Username)
+            .HasColumnName("username")
+            .HasMaxLength(50)
+            .IsRequired();
+
+        user.Property(entity => entity.Email)
+            .HasColumnName("email")
+            .HasMaxLength(254)
+            .IsRequired();
+
+        user.Property(entity => entity.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        user.HasIndex(entity => entity.Username)
             .IsUnique();
 
-        modelBuilder.Entity<Rating>()
-            .HasOne(rating => rating.User)
+        user.HasIndex(entity => entity.Email)
+            .IsUnique();
+    }
+
+    private static void ConfigureMovies(ModelBuilder modelBuilder)
+    {
+        var movie = modelBuilder.Entity<Movie>();
+
+        movie.ToTable("movies");
+
+        movie.HasKey(entity => entity.MovieId);
+
+        movie.Property(entity => entity.MovieId)
+            .HasColumnName("movie_id")
+            .ValueGeneratedOnAdd();
+
+        movie.Property(entity => entity.Title)
+            .HasColumnName("title")
+            .HasMaxLength(255)
+            .IsRequired();
+
+        movie.Property(entity => entity.Summary)
+            .HasColumnName("summary")
+            .HasMaxLength(5000);
+
+        movie.Property(entity => entity.ReleaseYear)
+            .HasColumnName("release_year")
+            .IsRequired();
+    }
+
+    private static void ConfigureRatings(ModelBuilder modelBuilder)
+    {
+        var rating = modelBuilder.Entity<Rating>();
+
+        rating.ToTable("ratings");
+
+        rating.HasKey(entity => entity.RatingId);
+
+        rating.Property(entity => entity.RatingId)
+            .HasColumnName("rating_id")
+            .ValueGeneratedOnAdd();
+
+        rating.Property(entity => entity.Score)
+            .HasColumnName("score")
+            .IsRequired();
+
+        rating.Property(entity => entity.Review)
+            .HasColumnName("review")
+            .HasMaxLength(2000);
+
+        rating.Property(entity => entity.RatedAt)
+            .HasColumnName("rated_at")
+            .IsRequired();
+
+        rating.Property(entity => entity.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+
+        rating.Property(entity => entity.MovieId)
+            .HasColumnName("movie_id")
+            .IsRequired();
+
+        rating.HasIndex(entity => new
+        {
+            entity.UserId,
+            entity.MovieId
+        })
+            .IsUnique();
+
+        rating.HasOne(entity => entity.User)
             .WithMany(user => user.Ratings)
-            .HasForeignKey(rating => rating.UserId)
+            .HasForeignKey(entity => entity.UserId)
             .OnDelete(DeleteBehavior.Cascade);
 
-        modelBuilder.Entity<Rating>()
-            .HasOne(rating => rating.Movie)
+        rating.HasOne(entity => entity.Movie)
             .WithMany(movie => movie.Ratings)
-            .HasForeignKey(rating => rating.MovieId)
+            .HasForeignKey(entity => entity.MovieId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        rating.ToTable(tableBuilder =>
+        {
+            tableBuilder.HasCheckConstraint(
+                "ck_ratings_score_range",
+                "\"score\" >= 1 AND \"score\" <= 10");
+        });
     }
 }
